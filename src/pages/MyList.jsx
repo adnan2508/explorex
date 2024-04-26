5948d8def1110f92357bd51241f87865982bd0f9
adnan2508 <adnandiu2800@gmail.com>
2024-04-27 03:31:27 +0600

    my list working on

--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const MyList = () => {
     return (
         <div>
             <Navbar></Navbar>
+
             <div className='w-11/12 mx-auto'>
                 <h1>Welcome this is your list!🎉</h1>
             </div>
+            
+            <Footer></Footer>
         </div>
     );
 };

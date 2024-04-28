32a42d2f0028ecebf4b248b9fdc3e224c300748b
adnan2508 <adnandiu2800@gmail.com>
2024-04-28 21:28:28 +0600

    two new package added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,42 @@ import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
 import { Helmet } from "react-helmet";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Home = () => {
+  const [text] = useTypewriter({
+    words: ["Partner", "Guide", "Companion", "Friend"],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 50,
+  });
+  // const handleType = (count: number) => {
+  //   // access word count number
+  //   console.log(count)}
+  // }
+
+  // const handleDone = () => {
+  //   console.log(`Done after 5 loops!`)
+  // }
+
   return (
     <div>
       <Helmet>
         <title>Explorex</title>
       </Helmet>
       <Navbar></Navbar>
+
       <div className="w-11/12 mx-auto mt-8">
+        <h2 className="text-center text-4xl font-bold" style={{ margin: "30px"}}
+        >
+          Explorex! Your Best Travel
+          <span style={{ color: "Blue", fontWeight: "bold", marginLeft: "13px" }}>
+            {text}
+          </span>
+          <span style={{ color: "red" }}>
+            <Cursor />
+          </span>
+        </h2>
         <Banner></Banner>
       </div>
 

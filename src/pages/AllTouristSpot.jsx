a6820b62cdd685969faec652349fdda7fea02bca
adnan2508 <adnandiu2800@gmail.com>
2024-04-28 18:24:51 +0600

    tourist spot added

--- src/pages/AllTouristSpot.jsx
+++ src/pages/AllTouristSpot.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import { Helmet } from 'react-helmet';
-import { useLoaderData } from 'react-router-dom';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import { Helmet } from "react-helmet";
+import { useLoaderData } from "react-router-dom";
+import AllSpotCard from "../components/AllSpotCard";
 
 const AllTouristSpot = () => {
-    const spots = useLoaderData();
-    return (
-        <div>
-            <Helmet>
-                <title>All Tourist Spot</title>
-            </Helmet>
-            <Navbar></Navbar>
-            <div className='w-11/12 mx-auto mt-10 mb-10'>
-            <h2 className='text-center text-5xl font-bold'>All tourist spot: {spots.length}</h2>
-            </div>
-            <Footer></Footer>
+  const spots = useLoaderData();
+  console.log(spots);
+  return (
+    <div>
+      <Helmet>
+        <title>All Tourist Spot</title>
+      </Helmet>
+      <Navbar></Navbar>
+
+      <div className="w-11/12 mx-auto mt-10 mb-10">
+        <h2 className="text-center text-5xl font-bold">
+          All tourist spot: {spots.length}
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-3">
+          {
+            spots.map(spot => <AllSpotCard key={spot._id} spot={spot}></AllSpotCard>)
+          }
         </div>
-    );
+
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default AllTouristSpot;

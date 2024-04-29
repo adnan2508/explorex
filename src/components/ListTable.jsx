58a84ac252f1e39671c0284867ec419be229ac0c
adnan2508 <adnandiu2800@gmail.com>
2024-04-29 23:20:07 +0600

    table done

---
+++ src/components/ListTable.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ListTable = ({ p }) => {
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+        
+              <th>Name</th>
+              <th>Location</th>
+              <th>Country</th>
+              <th>Average Cost</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+            
+              <td>{p.spotName}</td>
+              <td>{p.location}</td>
+              <td>{p.country}</td>
+              <td>${p.cost}</td>
+              <td className="flex flex-col gap-3">
+                <button className="btn btn-info text-white">Update</button>
+                <button className="btn btn-error text-white">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ListTable;
--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,33 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import useAuth from '../hooks/useAuth';
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import useAuth from "../hooks/useAuth";
+import ListTable from "../components/ListTable";
 
 const MyList = () => {
-    const {user} = useAuth() || {};
-    const [item, setItem] = useState([]);
-    console.log(user);
+  const { user } = useAuth() || {};
+  const [item, setItem] = useState([]);
+  console.log(user);
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/myList/${user?.email}`)
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            setItem(data);
-        })
-    }, [user])
+  useEffect(() => {
+    fetch(`http://localhost:5000/myList/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setItem(data);
+        console.log(data);
+      });
+  }, [user]);
 
-    return (
-        <div>
-            <Navbar></Navbar>
+  return (
+    <div>
+      <Navbar></Navbar>
 
-            <div className='w-11/12 mx-auto my-10'>
-                <h1>Welcome this is your list!🎉</h1>
-            </div>
-            
-            <Footer></Footer>
-        </div>
-    );
+      <div className="w-11/12 mx-auto my-10">
+        {
+        item?.map((p) => <ListTable p={p}></ListTable>)
+        }
+      </div>
+
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MyList;
